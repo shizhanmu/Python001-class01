ea84be70cde7c28dafbd7c1857e26b31f0c9cb3f
shizhanmu <tert1@163.com>
2020-07-13 09:06:03 +0800

    Add unique in mysql table

--- week03/lagou/create_table.sql
+++ week03/lagou/create_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE `position`  (
   `city` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   `salary` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `city`(`city`, `title`, `salary`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
